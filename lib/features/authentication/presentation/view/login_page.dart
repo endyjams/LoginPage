b8626f021d06728e975c65883672f2c76ed5d6ce
Endy José Alexandre Melo dos Santos <endy.jams@Endys-MBP.local>
2023-11-16 19:18:10 -0300

    feat! & permissions!: adding launchUrl feature with url_launcher package & adding query_all_packages permission on AndroidManifest

--- lib/features/authentication/presentation/view/login_page.dart
+++ lib/features/authentication/presentation/view/login_page.dart
@@ -1,14 +1,31 @@
 import 'package:flutter/material.dart';
 import 'package:login_page/features/authentication/presentation/widgets/login_form.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class LoginPage extends StatelessWidget {
+  Future<void> _launchURL(Uri url) async {
+    if (await canLaunchUrl(url)) {
+      await launchUrl(
+        url,
+        mode: LaunchMode.inAppBrowserView
+      );
+    } else {
+      debugPrint('Could not launch $url');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Login')),
-      body: Padding(
-        padding: EdgeInsets.all(8.0),
-        child: LoginForm(),
+      body: Column(
+        children: [
+          LoginForm(),
+          InkWell(
+            child: Text('Política de Privacidade'),
+            onTap: () => _launchURL(Uri.parse('https://google.com')),
+          )
+        ],
       ),
     );
   }
--- lib/features/authentication/presentation/widgets/login_form.dart
+++ lib/features/authentication/presentation/widgets/login_form.dart
@@ -3,16 +3,12 @@ import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:login_page/features/authentication/state/login_store.dart';
 import 'package:provider/provider.dart';
 
-class LoginForm extends StatefulWidget {
-  @override
-  _LoginFormState createState() => _LoginFormState();
-}
-
-class _LoginFormState extends State<LoginForm> {
+class LoginForm extends StatelessWidget {
+  const LoginForm({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final _loginStore = Provider.of<LoginStore>(context);
+    final loginStore = Provider.of<LoginStore>(context);
 
     return Form(
       child: Column(
@@ -20,34 +16,43 @@ class _LoginFormState extends State<LoginForm> {
           Observer(
             builder: (_) => TextFormField(
               onChanged: (value) {
-                _loginStore.username = value;
-                _loginStore.validateUsername();
+                loginStore.username = value;
+                loginStore.validateUsername();
               },
               decoration: InputDecoration(
                 labelText: 'Username',
-                errorText: _loginStore.usernameError,
+                errorText: loginStore.usernameError,
               ),
             ),
           ),
           Observer(
             builder: (_) => TextFormField(
               onChanged: (value) {
-                _loginStore.password = value;
+                loginStore.password = value;
               },
               obscureText: true,
               decoration: InputDecoration(
                 labelText: 'Password',
-                errorText: _loginStore.passwordError,
+                errorText: loginStore.passwordError,
               ),
             ),
           ),
           Observer(
             builder: (_) => ElevatedButton(
               onPressed: () {
-                _loginStore.validateUsername();
-                _loginStore.validatePassword();
+                loginStore.validateUsername();
+                loginStore.validatePassword();
+                debugPrint('User tried to login');
               },
-              child: const Text('Login'),
+              style: ElevatedButton.styleFrom(
+                shape: const StadiumBorder(),
+                backgroundColor: Colors.green[400],
+                padding: EdgeInsets.symmetric(
+                  horizontal: MediaQuery.of(context).size.width * 0.16,
+                  vertical: MediaQuery.of(context).size.height * 0.0185,
+                ),
+              ),
+              child: const Text('Entrar'),
             ),
           ),
           // Add more widgets as needed
